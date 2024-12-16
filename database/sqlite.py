7c330932c3917eef358f037319c29747f54bbcd9
Yuda <yuda_p411@apps.ipb.ac.id>
2024-12-16 21:02:02 +0700

    16/12/2024

--- database/sqlite.py
+++ database/sqlite.py
@@ -4,9 +4,10 @@ import sqlite3
 def init_db():
     db_path = os.path.join(os.path.dirname(__file__), '../rest_api_py.db')
     conn = sqlite3.connect(os.path.abspath(db_path))
-    c = conn.cursor()
-    c.execute('''DROP TABLE IF EXISTS users''')
+    drop_users_table_sql = conn.cursor()
+    drop_users_table_sql.execute('''DROP TABLE IF EXISTS users''')
     conn.commit()
-    c.execute('''CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT NOT NULL)''')
+    create_users_table_sql = conn.cursor()
+    create_users_table_sql.execute('''CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT NOT NULL)''')
     conn.commit()
     conn.close()
